29d96e634a9cb1a1487db149e937dc273227ac2a
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-08-20 19:43:32 +0100

    Update __init__.py

--- app/bot1/__init__.py
+++ app/bot1/__init__.py
@@ -1,6 +1,7 @@
 # app/bot1/__init__.py
 from flask import Blueprint
 
-bot1_bp = Blueprint('bot1', __name__, template_folder='templates', static_folder='static', data_folder='data')
+bot1_bp = Blueprint('bot1', __name__, template_folder='templates', static_folder='static')
+
 
 from . import routes

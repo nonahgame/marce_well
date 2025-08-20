c544eed688a5de48a7e1502b27032c77da22862e
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-08-20 20:27:57 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 # app/__init__.py
 from flask import Flask
-from app.bot1.routes import bot1_bp
-#from app.todo.routes import todo_bp
-#from app.bot2.routes import bot2_bp
+from app.bot1.routes import bot1
+from app.todo.routes import todo
+from app.bot2.routes import bot2
 
 def create_app():
     app = Flask(__name__)
@@ -10,8 +10,9 @@ def create_app():
 
     # Register blueprints with URL prefixes
     app.register_blueprint(bot1_bp, url_prefix='/bot1')
-    #app.register_blueprint(todo_bp, url_prefix='/todo')
-    #app.register_blueprint(bot2_bp, url_prefix='/bot2')
+    app.register_blueprint(todo_bp, url_prefix='/todo')
+    app.register_blueprint(bot2_bp, url_prefix='/bot2')
 
 
     return app
+

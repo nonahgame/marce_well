9339650b7252d3e70ee36a71d271ab0b129ca09d
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-08-20 19:45:09 +0100

    Update __init__.py

--- app/todo/__init__.py
+++ app/todo/__init__.py
@@ -1,6 +1,7 @@
 # app/todo/__init__.py
 from flask import Blueprint
 
-todo_bp = Blueprint('todo', __name__, template_folder='templates', static_folder='static', data_folder='data')
+todo_bp = Blueprint('todo', __name__, template_folder='templates', static_folder='static')
+
 
 from . import routes

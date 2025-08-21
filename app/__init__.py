59220529e772c98e7983d4a226be80c520cd77b4
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-08-21 13:35:24 +0100

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+# app/__init__.py
+from flask import Flask
+from app.bot1 import bot1_bp
+from app.todo import todo_bp
+from app.bot2 import bot2_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    # Register blueprints with URL prefixes
+    app.register_blueprint(bot1_bp, url_prefix='/bot1')
+    app.register_blueprint(todo_bp, url_prefix='/todo')
+    app.register_blueprint(bot2_bp, url_prefix='/bot2')
+
+    return app
